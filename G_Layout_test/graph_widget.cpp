1217556da983df60002cfdc39320242609e35711
FUJI0130 <38177738+FUJI0130@users.noreply.github.com>
2020-05-13 10:00:13 +0900

    Update graph_widget.cpp

--- G_Layout_test/graph_widget.cpp
+++ G_Layout_test/graph_widget.cpp
@@ -31,62 +31,6 @@ Graph_Widget::Graph_Widget(QGraphicsWidget *parent) : QGraphicsWidget(parent, Qt
 }
 
 
-/*
-
-void Graph_Widget::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
-    QWidget *widget)
-{
-    Q_UNUSED(widget);
-    Q_UNUSED(option);
-    QRectF frame(QPointF(0, 0), geometry().size());
-
-    double widget_geom_width = frame.width();
-    double widget_geom_height = frame.height();
-    double widget_geom_pos_right = frame.right();
-    double widget_geom_pos_left = frame.left();
-
-    qDebug("widget_geom_width : %3.2lf\n",widget_geom_width);
-    qDebug("widget_geom_height : %3.2lf\n",widget_geom_height);
-    qDebug("widget_geom_pos_right : %3.2lf\n",widget_geom_pos_right);
-    qDebug("widget_geom_pos_left: %3.2lf\n",widget_geom_pos_left);
-
-    QPointF pixpos = QPointF(0,0);
-
-
-}
-
-QRectF Graph_Widget::boundingRect() const
-{
-    qDebug("Graph_Widget boundingRect\n");
-    return QRectF(QPointF(0, 0), geometry().size());
-    //return QRectF(QPointF(0, 0), QSizeF(0,0));
-}
-
-void Graph_Widget::setGeometry(const QRectF &geom)
-{
-    qDebug("Graph_Widget setGeometry\n");
-    prepareGeometryChange();
-    //QGraphicsLayoutItem::setGeometry(geom);
-    QGraphicsLayoutItem::setGeometry(geom_test);
-    //setPos(geom.topLeft());
-    setPos(geom_test.topLeft());
-}
-
-QSizeF Graph_Widget::sizeHint(Qt::SizeHint which, const QSizeF &constraint) const
-{
-     qDebug("Graph_Widget sizeHint\n");
-    switch (which) {
-    case Qt::MinimumSize:
-    case Qt::PreferredSize:
-        return geom_test.size();
-    case Qt::MaximumSize:
-        return QSizeF(1000, 1000);
-    default:
-        break;
-    }
-    return constraint;
-}
-*/
 
 void Graph_Widget::set_Item()
 {
